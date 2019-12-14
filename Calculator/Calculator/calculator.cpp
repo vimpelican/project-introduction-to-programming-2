dd8c86d380346bb2cabf09acc2af7a28ceb49a6a
snustcice <snustcice@seoultech.ac.kr>
2019-12-14 14:16:52 +0900

    Update flowchart, header, source code

--- Calculator/Calculator/calculator.cpp
+++ Calculator/Calculator/calculator.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void Initialize()
 {
 	srand((unsigned int)time(NULL));
-	system("mode con cols=60 lines=30 | title Calculator");
+	system("mode con cols=60 lines=20 | title Calculator");
 
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);	//Get ConsoleHandle
 	CONSOLE_CURSOR_INFO ConsoleCursor;
@@ -23,7 +23,6 @@ void MoveCursor(int x, int y)
 	SetConsoleCursorPosition(consoleHandle, pos);
 }
 
-
 int ReadKey(void)
 {
 	int temp = _getch();
@@ -40,8 +39,9 @@ int ReadKey(void)
 		return KEY_RIGHT;
 	case 10:
 		return SUBMIT;
+	case 27:
+		return ESC;
 	default:
-		return 0;
 		break;
 	}
 }
@@ -61,63 +61,74 @@ void ShowTitle(float Seconds)
 	system("cls");
 }
 
-int ShowMode(void)
+void ShowMode(void)
 {
-	MoveCursor(0, 2);
-	for (int i = 0; i < 60; i++)
+	MoveCursor(0, 1);
+	for (int i = 0; i < COLS; i++)
+		cout << "-";
+	MoveCursor(0, 18);
+	for (int i = 0; i < COLS; i++)
 		cout << "-";
 
-	MoveCursor(3, 7);
-	cout << ">";
-	MoveCursor(4, 7);
-	cout << "1. Calculate(기본적인 계산)";
-	MoveCursor(4, 8);
-	cout << "2. Complex(복소수 계산)";
-	MoveCursor(4, 9);
-	cout << "3. Base-N(진수 변환)";
-	MoveCursor(4, 10);
-	cout << "4. Matrix(행렬 연산)";
+	MoveCursor(24, 3);
+	cout << "<조작 안내>";
+	MoveCursor(2, 4);
+	cout << "방향키로 위/아래를 이동하고 엔터 키를 이용해 선택합니다.";
 
-	return 0;
+	int x = 7, y = 6;
+	MoveCursor(x - 2, y);
+	cout << "1. Calculate(기본적인 계산)";
+	MoveCursor(x - 2, y + 2);
+	cout << "2. Complex(복소수 계산)";
+	MoveCursor(x - 2, y + 4);
+	cout << "3. Base-N(진수 변환)";
+	MoveCursor(x - 2, y + 6);
+	cout << "4. Matrix(행렬 연산)";
+	MoveCursor(x - 2, y + 8);
+	cout << "5. 프로그램 종료";
 }
 
-/*int SelectMenu(void)
+/*int SelectMode(void)
 {
-	int x = 25, y = 12;
+	int x = 7, y = 6;
 	while (1)
 	{
 		int n = ReadKey();
 		switch (n) {
 		case KEY_UP:
 		{
-			if (y > 12)
+			if (y > 6)
 			{
-				MoveCursor(x - 2, y);
+				MoveCursor(x - 3, y);
 				cout << " ";
-				MoveCursor(x - 2, y -= 2);
+				MoveCursor(x - 3, y -= 2);
 				cout << ">";
 			}
 		}
 		break;
 		case KEY_DOWN:
 		{
-			if (y < 16)
+			if (y < 14)
 			{
-				MoveCursor(x - 2, y);
+				MoveCursor(x - 3, y);
 				cout << " ";
-				MoveCursor(x - 2, y += 2);
+				MoveCursor(x - 3, y += 2);
 				cout << ">";
 			}
 		}
 		break;
 		case SUBMIT:
 		{
-			if (y == 12)
-				return GAME_START;
+			if (y == 6)
+				return CALCULATE;
+			else if (y == 8)
+				return COMPLEX;
+			else if (y == 10)
+				return BASE_N;
+			else if (y == 12)
+				return MATRIX;
 			else if (y == 14)
-				return GAME_INFO;
-			else if (y == 16)
-				return GAME_QUIT;
+				return QUIT;
 		}
 		break;
 		}
--- Calculator/Calculator/main.cpp
+++ Calculator/Calculator/main.cpp
@@ -1,10 +1,118 @@
 #include "calculator.h"
 
-int main(void)
+/*int main(void)
 {
 	Initialize();
 	ShowTitle(1);
 	ShowMode();
+	while (1)
+	{
+		switch (SelectMode())
+		{
+		case CALCULATE:
+			break;
+		case TRIGONOMETRIC:
+			break;
+		case BASE_N:
+			break;
+		case MATRIX:
+			break;
+		case QUIT:
+			break;
+		default:
+			break;
+		}
+		system("cls");
+	}
+	getchar();
+	return 0;
+}
+*/
+// String Calculator
+#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+
+struct oper {
+	int p; // 연산자 우선순위
+	string o; // 연산자
+};
+
+stack<int> num; // 숫자 스택
+stack<oper> op; // 연산자 스택
+
+void calc() {
+	int a, b, result;
+	b = num.top();
+	num.pop();
+	a = num.top();
+	num.pop();
+	string oper = op.top().o;
+	op.pop();
+
+	if (oper == "*")
+		result = a * b;
+	else if (oper == "/")
+		result = a / b;
+	else if (oper == "+")
+		result = a + b;
+	else if (oper == "-")
+		result = a - b;
+	// 결과 값 스택에 다시 저장
+	num.push(result);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string input = "15 + 32 * ( 1 - 8 ) / 2"; // -97
+	stringstream ss(input);
+
+	// 연산자 우선순위에 따라 스택에 push
+	// 0 : ( )
+	// 1 : + -
+	// 2 : * /
+	string tok;
+	while (ss >> tok) {
+		// ( 는 무조건 연산자 스택에 push
+		if (tok == "(") {
+			op.push({ 0, tok });
+		} // ) 가 나오면 ( 가 나올 때 까지 계산
+		else if (tok == ")") {
+			while (op.top().o != "(")
+				calc();
+			op.pop();
+		}
+		else if (tok == "*" || tok == "/" || tok == "+" || tok == "-") {
+			int prior; // 연산자 우선순위
+			if (tok == "*")
+				prior = 2;
+			else if (tok == "/")
+				prior = 2;
+			else if (tok == "+")
+				prior = 1;
+			else if (tok == "-")
+				prior = 1;
+
+			// 연산자 우선 순위 낮은게 top으로 올 때까지 계산
+			while (!op.empty() && prior <= op.top().p)
+				calc();
+			// 스택에 연산자 push
+			op.push({ prior, tok });
+		}
+		else // 숫자일 경우 숫자 스택에 push
+			num.push(stoi(tok));
+	}
+	// 남은 연산자 계산
+	while (!op.empty())
+		calc();
+
+	cout << num.top();
 	getchar();
 	return 0;
 }
