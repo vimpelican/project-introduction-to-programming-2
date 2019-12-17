448ea25cba34c726726b12350d9e8a152dd9bfb6
AzureSoda <dmb07250@gmail.com>
2019-12-17 15:13:25 +0900

    fix base_n.h and fuctions

--- Calculator/Calculator/base_n.h
+++ Calculator/Calculator/base_n.h
@@ -5,43 +5,8 @@ class Base_N : public Calculator
 private:
 
 public:
-	void ChangeDecimal(int binary)
-	{
-		int sum = 0;
-		int temp = 1;
-		while (binary != 0)
-		{
-			if (binary % 10 != 0 && binary % 10 != 1)
-			{
-				printf("잘못입력했습니다");
-				return;
-			}
-
-			if (binary % 10 != 0)
-			{
-				sum = sum + temp;
-			}
-			binary = binary / 10;
-			temp = temp * 2;
-		}
-		printf("Answer is %d\n", sum);
-
-	}
-	void ChangeBinary(int decimal) {
-			int i = 99; char c[17] = "0123456789ABCDEF", result[100];
-
-			result[i] = '\0'; // i=99 
-			while (decimal != 0) {
-				result[--i] = c[decimal % 2]; // result[] <-- 나머지 
-				decimal /= 2; // 몫 
-			}
-			printf("Answer is %s\n", &result[i]);
-
-		}
-
-
-
-
+	void ChangeDecimal(int binary) {} //선언
+	void ChangeBinary(int decimal) {} // 선언
 };
 
 /*
